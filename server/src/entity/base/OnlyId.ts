939e3c6b1d0b8f49157ef10d0eda6fb62827401a
Ren0503 <illuminati0440@gmail.com>
2021-12-07 20:53:21 +0700

    Edit framework

--- server/src/entity/base/OnlyId.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType, Field, ID } from 'type-graphql';
-import { BaseEntity, PrimaryGeneratedColumn } from 'typeorm';
-
-@ObjectType()
-export abstract class OnlyId extends BaseEntity {
-    @Field(() => ID)
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-}
