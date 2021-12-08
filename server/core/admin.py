3e6cf97e4ce3547fe3943d7e91c0237d2999de53
Ren0503 <illuminati0440@gmail.com>
2021-12-08 19:48:40 +0700

    Add schema

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Business)
+admin.site.register(Product)
+admin.site.register(ProductComment)
+admin.site.register(ProductImage)
+admin.site.register(Wish)
+admin.site.register(Cart)
+admin.site.register(RequestCart)
